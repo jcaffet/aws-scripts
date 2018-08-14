5f34ce629858c1cf6a00d7102bdc5b16df4d2c04
Jérôme CAFFET <jerome.caffet@carboatmedia.fr>
2018-08-14 13:36:22 +0200

    add script to retrieve ASG per tag values

---
+++ scripts/asg/list_asg_by_tag.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# this script simply gives the list of the current account Auto Scaling Groups (ASG) depending on one key/value tag
+# The script uses JMESPath as query language as query language
+import boto3
+import sys
+
+def main(argv):
+
+  TAG_KEY = 'someKey'
+  TAG_VALUE = 'someValue'
+
+  client = boto3.client('autoscaling')
+  paginator = client.get_paginator('describe_auto_scaling_groups')
+  page_iterator = paginator.paginate(
+      PaginationConfig={'PageSize': 100}
+  )
+
+  filtered_asgs = page_iterator.search(
+      'AutoScalingGroups[] | [?contains(Tags[?Key==`{}`].Value, `{}`)]'.format(TAG_KEY,TAG_VALUE))
+  
+  for asg in filtered_asgs:
+      print asg['AutoScalingGroupName']
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
