0c4e9c498d129dba5fd24d17f3e5929819e68932
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-03-21 09:51:43 +0100

    performance improvements

--- scripts/list_instances_with_unvisible_ami.py
+++ scripts/list_instances_with_unvisible_ami.py
@@ -1,29 +1,30 @@
 #!/usr/bin/python
-# This script displays the list of the EC2 instances for which it is not possible to load details.
+# This script displays the list of the EC2 instances for which it is not possible to load their AMI.
 # Maybe because the AMI has been deregisted or permissions have changed
 # This will cause major unpleasant effects for any change on the EC2 lifecycle.
-
+# This script lists all ec2 instances of an account and a region : id;name;stack_name;image_id
 import boto3
 import boto3.ec2
 import sys
 
-# This script lists all ec2 instances of an account : id;name;stack_name;image_id
-
 def main(argv):
-    ec2 = boto3.resource('ec2')
     
-    instances = ec2.instances.all()
-    instance_ami_ids=list([instance.image_id for instance in instances])
+    ec2 = boto3.resource('ec2')
 
-    visible_images = ec2.images.filter(ImageIds=instance_ami_ids)
-    visible_image_ids=list([image.image_id for image in visible_images])
+    allInstances = ec2.instances.all()
+    allInstanceAmiIds=list([instance.image_id for instance in allInstances])
 
-    for instance in instances:
-       if instance.image_id not in visible_image_ids:
-          print("%s;%s;%s" % (instance.instance_id,instance.image_id,get_instance_name(instance)))
+    # check first if allInstanceAmiIds is empty otherwise ec2.images.filter will retreive all image and take long time
+    if allInstanceAmiIds:
+        # the following filter will only return AMI information of the visible AMI in parameter and will forget the others
+        allInstanceUniqueAmiIds = list(set(allInstanceAmiIds))
+        visibleInstanceAmis = ec2.images.filter(ImageIds=allInstanceUniqueAmiIds)
+        visibleInstanceAmiIds = list([image.image_id for image in visibleInstanceAmis])
+        for instance in allInstances:
+            if instance.image_id not in visibleInstanceAmiIds:
+                print("%s;%s;%s" % (instance.instance_id, instance.image_id, getInstanceName(instance)))
 
-
-def get_instance_name(ec2_instance):
+def getInstanceName(ec2_instance):
     instance_name=''
     if ec2_instance.tags:
         for tag in ec2_instance.tags:
