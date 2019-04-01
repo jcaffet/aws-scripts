8ed32d54231dbb3ddaf6d52c516f3c0a519ad9ca
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-04-01 22:42:41 +0200

    minor improvments

--- iam/list_inactive_iam_users.py
+++ iam/list_inactive_iam_users.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# This script displays a list of unsed users
+# This script displays a list of unsed IAM users
 # They match the folling conditions :
 # - user is older than MAX_ACTIVITY_AGE
 # - no password access older than MAX_ACTIVITY_AGE
@@ -10,28 +10,24 @@ import datetime,time
 
 def main(argv):
 
-   MAX_ACTIVITY_AGE=365
-
-   iam_resource = boto3.resource('iam')
-   iam_client = boto3.client('iam')
-
-   all_users = iam_resource.users.all()
-
-   for user in all_users:
-
-      most_recent_accesskey_age = get_older_accesskey_usage(user)  
-      user_age = number_of_days_since_now(user.create_date)
-      last_password_login_age = number_of_days_since_now(user.password_last_used)
-
-      if    (last_password_login_age > MAX_ACTIVITY_AGE or last_password_login_age == -1) \
-        and (most_recent_accesskey_age > MAX_ACTIVITY_AGE or most_recent_accesskey_age == -1) \
-        and (user_age > MAX_ACTIVITY_AGE):
-        print("%s;%s;%s;%s" % (user.user_name, user_age, last_password_login_age,most_recent_accesskey_age))
-
-
-def get_older_accesskey_usage(user):
+  MAX_ACTIVITY_AGE=365
 
   iam_client = boto3.client('iam')
+  iam_resource = boto3.resource('iam')
+  all_users = iam_resource.users.all()
+
+  for user in all_users:
+    most_recent_accesskey_usage_age = get_older_accesskey_usage_age(iam_client, user)
+    user_age = number_of_days_since_now(user.create_date)
+    last_password_login_age = number_of_days_since_now(user.password_last_used)
+
+    if  (last_password_login_age > MAX_ACTIVITY_AGE or last_password_login_age == -1) \
+        and (most_recent_accesskey_usage_age > MAX_ACTIVITY_AGE or most_recent_accesskey_usage_age == -1) \
+        and (user_age > MAX_ACTIVITY_AGE):
+      print("%s;%s;%s;%s" % (user.user_name, user_age, last_password_login_age, most_recent_accesskey_usage_age))
+
+
+def get_older_accesskey_usage_age(iam_client, user):
 
   recent_accesskey_age = -1
   for key in user.access_keys.all():
