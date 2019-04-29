039a0d7b4234f2379508df46a2aa42adb3f144ee
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-04-29 16:46:55 +0200

    Update script to python3.7

--- iam/list_iam_users_old_password.py
+++ iam/list_iam_users_old_password.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python
 # This script simply gives a list of IAM users with a password age older the MAX_PASSWORD_AGE
+# Pay attention that credential reports are renewed by AWS every 4 hours.
+# Credential Reports provide date in ISO8601 format like 2017-03-01T09:51:51+00:00
+# To convert to datetime, with use an out of the box function new in Python 3.7
 import boto3
+from botocore.exceptions import ClientError
 import sys
-import datetime,time
+import datetime
+from datetime import datetime, timezone
 from time import sleep
 import csv
+import base64
+
 
 def main(argv):
 
@@ -22,34 +29,27 @@ def main(argv):
 def get_credential_report(iam_client):
     resp = iam_client.generate_credential_report()
     if resp['State'] == 'COMPLETE' :
-        try: 
+        try:
             response = iam_client.get_credential_report()
             credential_report_csv = response['Content']
-            reader = csv.DictReader(credential_report_csv.splitlines())
-            # print(reader.fieldnames)
+            reader = csv.DictReader(credential_report_csv.decode('utf-8').splitlines())
             credential_report = []
             for row in reader:
                 credential_report.append(row)
             return(credential_report)
         except ClientError as e:
-            print("Unknown error getting Report: " + e.message)
+            print("Error getting Report: " + e.message)
     else:
         sleep(2)
         return get_credential_report(iam_client)
 
 def number_of_days_since_now(mydate):
-
-  if mydate :
-     now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-
-     mydate_dec = time.mktime(datetime.datetime.strptime(mydate, "%Y-%m-%dT%H:%M:%S+00:00").timetuple())
-     now_dec = time.mktime(datetime.datetime.strptime(now, "%Y-%m-%d %H:%M:%S").timetuple())
-
-     active_days = (now_dec - mydate_dec)/60/60/24
-     return int(round(active_days))
-  else:
-    return -1
+    # The incoming mydate is in String type and could contain "no_information" values
+    # fromisoformat only exists since Python 3.7
+    if mydate == "no_information":
+        return mydate
+    else:
+        return (datetime.now(timezone.utc) - datetime.fromisoformat(mydate)).days
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
