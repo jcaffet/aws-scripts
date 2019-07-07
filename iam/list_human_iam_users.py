311850ca8c913fc933324ceca59052b20a103e0a
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-07-07 15:26:29 +0200

    Update to Python 3.6 and higher

--- iam/list_human_iam_users.py
+++ iam/list_human_iam_users.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # This script simply gives a list of human IAM users (IAM users with a password)
+# Need Python >= 3.6
 import boto3
 import sys
 from time import sleep
@@ -8,29 +9,29 @@ import csv
 def main(argv):
 
    iam_client = boto3.client('iam')
-   credential_report = get_credential_report(iam_client)
+   AwsAccountId = boto3.client('sts').get_caller_identity()['Account']
 
+   credential_report = get_credential_report(iam_client)
    for row in credential_report:
      if row['password_enabled'] == "true":
-       print ("%s;%s" % (row['user'], row['password_last_used']))
+       print ("%s;%s;%s" % (AwsAccountId, row['user'], row['password_last_used']))
 
 def get_credential_report(iam_client):
     resp = iam_client.generate_credential_report()
     if resp['State'] == 'COMPLETE' :
-        try: 
+        try:
             response = iam_client.get_credential_report()
             credential_report_csv = response['Content']
-            reader = csv.DictReader(credential_report_csv.splitlines())
+            reader = csv.DictReader(credential_report_csv.decode('utf-8').splitlines())
             credential_report = []
             for row in reader:
                 credential_report.append(row)
             return(credential_report)
         except ClientError as e:
-            print("Unknown error getting Report: " + e.message)
+            print("Error getting Report: " + e.message)
     else:
         sleep(2)
         return get_credential_report(iam_client)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
