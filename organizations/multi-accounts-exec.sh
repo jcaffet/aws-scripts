5e04bb9eb2b20ad316019d201970a94f85249db8
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-07-07 15:32:44 +0200

    Improve multi-account script syntax

--- organizations/multi-accounts-exec.sh
+++ organizations/multi-accounts-exec.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 # The aim of the script is the get a role on each account of the AWS Organizations service
+# Do nto forget to specify your parameters (ROLE and IDENTITY_ACCOUNT)
 ROLE=Administrator
 TMP_ROLE_FILE=/tmp/assumerole.json
-IDENTITY_ACCOUNT=123456123456
+IDENTITY_ACCOUNT=
 accounts=`aws organizations list-accounts | jq -r '.Accounts[] | select(.Status=="ACTIVE") | .Id'`
 
 
 command-to-exec () {
-  python3.7 iam/disable-unused-password.py
+  # your command here
+
 }
 
 for account in ${accounts}; do
   if [ "${IDENTITY_ACCOUNT}" != "${account}" ]; then
-    echo "Get role on ${account}"
     aws sts assume-role \
             --role-arn arn:aws:iam::${account}:role/${ROLE} \
-            --role-session-name "RoleSession-${account}" > ${TMP_ROLE_FILE}
+            --role-session-name "RoleSession-${account}" \
+	    > ${TMP_ROLE_FILE}
     export AWS_SECRET_ACCESS_KEY=`cat ${TMP_ROLE_FILE} | jq -r .Credentials.SecretAccessKey`
     export AWS_ACCESS_KEY_ID=`cat ${TMP_ROLE_FILE} | jq -r .Credentials.AccessKeyId`
     export AWS_SESSION_TOKEN=`cat ${TMP_ROLE_FILE} | jq -r .Credentials.SessionToken`
     if [ -n "${AWS_SECRET_ACCESS_KEY}" ] && [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SESSION_TOKEN}" ]; then
-       # you might include an aws cli command here ...
        command-to-exec 
     fi
     rm ${TMP_ROLE_FILE}
@@ -28,7 +29,6 @@ for account in ${accounts}; do
     unset AWS_ACCESS_KEY_ID
     unset AWS_SESSION_TOKEN
   else
-    echo "Get role on ${account}"
     command-to-exec 
   fi
 done
