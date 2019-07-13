d84469dd1aec9dca9b8834666eb2056b5a3aa130
Jerome Caffet <15911786+jcaffet@users.noreply.github.com>
2019-07-13 21:52:51 +0200

    Add multi account multi region exec

--- organizations/multi-accounts-regions-exec.sh
+++ organizations/multi-accounts-regions-exec.sh
@@ -5,15 +5,15 @@ ROLE=Administrator
 TMP_ROLE_FILE=/tmp/assumerole.json
 IDENTITY_ACCOUNT=
 accounts=`aws organizations list-accounts | jq -r '.Accounts[] | select(.Status=="ACTIVE") | .Id'`
+regions=`aws ec2 describe-regions | jq -r '.Regions[] | .RegionName'`
 
 command-to-exec () {
   # your command here
-  echo "${account}"
+  echo "${account}:${region}"
 }
 
 for account in ${accounts}; do
   if [ "${IDENTITY_ACCOUNT}" != "${account}" ]; then
-    echo "${account}"
     aws sts assume-role \
             --role-arn arn:aws:iam::${account}:role/${ROLE} \
             --role-session-name "RoleSession-${account}" \
@@ -22,13 +22,16 @@ for account in ${accounts}; do
     export AWS_ACCESS_KEY_ID=`cat ${TMP_ROLE_FILE} | jq -r .Credentials.AccessKeyId`
     export AWS_SESSION_TOKEN=`cat ${TMP_ROLE_FILE} | jq -r .Credentials.SessionToken`
     if [ -n "${AWS_SECRET_ACCESS_KEY}" ] && [ -n "${AWS_ACCESS_KEY_ID}" ] && [ -n "${AWS_SESSION_TOKEN}" ]; then
-       echo "${account}"
-       command-to-exec
+       for region in ${regions}; do
+           AWS_DEFAULT_REGION=${region}
+           command-to-exec
+       done
     fi
     rm ${TMP_ROLE_FILE}
     unset AWS_SECRET_ACCESS_KEY
     unset AWS_ACCESS_KEY_ID
     unset AWS_SESSION_TOKEN
+    unset AWS_DEFAULT_REGION
   else
     command-to-exec
   fi
