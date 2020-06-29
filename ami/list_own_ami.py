7cc753d6279d87c1439186390e75ad09ac65b2e9
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-06-29 22:26:59 +0200

    improve fields and syntax

--- ami/list_own_ami.py
+++ ami/list_own_ami.py
@@ -1,18 +1,22 @@
-#!/usr/bin/python
-# This script lists all the AMIs of your own account with some additional details
-
+'''
+This script lists all the AMIs of your own account with some additional details
+'''
+import sys
 import boto3
 import boto3.ec2
-import sys
+
 
 def main(argv):
-    ec2 = boto3.resource('ec2')
 
+    ec2 = boto3.resource('ec2')
     images = ec2.images.filter(Owners=['self'])
 
     for image in images:
-        print("%s;%s;%s:%s" % (image.image_id,image.owner_id,image.name,image.description))
+        print("%s;%s;%s:%s" % (image.image_id,
+                               image.owner_id,
+                               image.name,
+                               image.description))
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
