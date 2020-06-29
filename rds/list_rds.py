0d4cffe1a9285582547e5781fc17fd1ad309ee3c
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-06-29 22:16:34 +0200

    code refacto with pylint

--- rds/list_rds.py
+++ rds/list_rds.py
@@ -1,22 +1,25 @@
-#!/usr/bin/python
-# This script gives the list of the RDS databases with some additional information
-
+'''
+This script gives the CSV list of the RDS databases
+'''
 import boto3
-import sys
 
 
-def main(argv):
-  client = boto3.client('rds')
-  response = client.describe_db_instances()
-  instances = response.get('DBInstances')
+def main():
+    '''
+    Simply connects to AWS API with boto3 and prints the result
+    '''
+    client = boto3.client('rds')
+    response = client.describe_db_instances()
+    instances = response.get('DBInstances')
 
-  for instance in instances:
-    db_identifier = instance.get('DBInstanceIdentifier')
-    db_instanceclass = instance.get('DBInstanceClass')
-    db_status = instance.get('DBInstanceStatus')
-    print("%s;%s;%s" % (db_identifier, db_instanceclass, db_status))
+    print("DBInstanceIdentifier;DBInstanceClass;DBInstanceStatus;PubliclyAccessible;StorageEncrypted")
+    for instance in instances:
+        print("%s;%s;%s;%s;%s" % (instance.get('DBInstanceIdentifier'),
+                                 instance.get('DBInstanceClass'),
+                                 instance.get('DBInstanceStatus'),
+                                 instance.get('PubliclyAccessible'),
+                                 instance.get('StorageEncrypted')))
 
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
-
+    main()
