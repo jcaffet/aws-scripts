1a9bd3859cc78021172f1bebc5477e03c7905d6d
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-06-29 15:40:02 +0200

    improvments with pylint

--- iam/list_inactive_iam_users.py
+++ iam/list_inactive_iam_users.py
@@ -1,62 +1,64 @@
-#!/usr/bin/python
-# This script displays a list of unsed IAM users
-# They match the folling conditions :
-# - user is older than MAX_ACTIVITY_AGE
-# - no password access older than MAX_ACTIVITY_AGE
-# - no accesskey usage older than MAX_ACTIVITY_AGE
-import boto3
+'''
+This script displays a list of unsed IAM users
+ They match the folling conditions :
+ - user is older than max_activity_age
+ - no password access older than max_activity_age
+ - no accesskey usage older than max_activity_age
+'''
 import sys
-import datetime,time
+import datetime
+import time
+import boto3
+
 
 def main(argv):
 
-  MAX_ACTIVITY_AGE=365
+    max_activity_age = 180
 
-  iam_client = boto3.client('iam')
-  iam_resource = boto3.resource('iam')
-  all_users = iam_resource.users.all()
+    iam_client = boto3.client('iam')
+    iam_resource = boto3.resource('iam')
+    all_users = iam_resource.users.all()
 
-  for user in all_users:
-    most_recent_accesskey_usage_age = get_older_accesskey_usage_age(iam_client, user)
-    user_age = number_of_days_since_now(user.create_date)
-    last_password_login_age = number_of_days_since_now(user.password_last_used)
+    print("user_name;user_age;last_password_login_age;most_recent_accesskey_usage_age")
+    for user in all_users:
+        most_recent_accesskey_usage_age = get_older_accesskey_usage_age(iam_client, user)
+        user_age = number_of_days_since_now(user.create_date)
+        last_password_login_age = number_of_days_since_now(user.password_last_used)
 
-    if  (last_password_login_age > MAX_ACTIVITY_AGE or last_password_login_age == -1) \
-        and (most_recent_accesskey_usage_age > MAX_ACTIVITY_AGE or most_recent_accesskey_usage_age == -1) \
-        and (user_age > MAX_ACTIVITY_AGE):
-      print("%s;%s;%s;%s" % (user.user_name, user_age, last_password_login_age, most_recent_accesskey_usage_age))
+        if (last_password_login_age > max_activity_age or last_password_login_age == -1) \
+            and (most_recent_accesskey_usage_age > max_activity_age or most_recent_accesskey_usage_age == -1) \
+            and (user_age > max_activity_age):
+            print("%s;%s;%s;%s" % (user.user_name, user_age, last_password_login_age, most_recent_accesskey_usage_age))
 
 
 def get_older_accesskey_usage_age(iam_client, user):
 
-  recent_accesskey_age = -1
-  for key in user.access_keys.all():
-    last_used_key = iam_client.get_access_key_last_used(AccessKeyId=key.id)
-    if 'LastUsedDate' in last_used_key['AccessKeyLastUsed']:
-      accesskeydate = last_used_key['AccessKeyLastUsed']['LastUsedDate']
-      accesskey_age = number_of_days_since_now(accesskeydate)
-      if recent_accesskey_age == -1 :
-        recent_accesskey_age = accesskey_age
-      else:
-        recent_accesskey_age = min(accesskey_age , recent_accesskey_age)
-  return recent_accesskey_age
+    recent_accesskey_age = -1
+    for key in user.access_keys.all():
+        last_used_key = iam_client.get_access_key_last_used(AccessKeyId=key.id)
+        if 'LastUsedDate' in last_used_key['AccessKeyLastUsed']:
+            accesskeydate = last_used_key['AccessKeyLastUsed']['LastUsedDate']
+            accesskey_age = number_of_days_since_now(accesskeydate)
+            if recent_accesskey_age == -1:
+                recent_accesskey_age = accesskey_age
+            else:
+                recent_accesskey_age = min(accesskey_age, recent_accesskey_age)
+    return recent_accesskey_age
 
 
 def number_of_days_since_now(mydate):
 
-  if mydate :
-     mydate = mydate.strftime("%Y-%m-%d %H:%M:%S")
-     now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+    if mydate:
+        mydate = mydate.strftime("%Y-%m-%d %H:%M:%S")
+        now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
-     mydate_dec = time.mktime(datetime.datetime.strptime(mydate, "%Y-%m-%d %H:%M:%S").timetuple())
-     now_dec = time.mktime(datetime.datetime.strptime(now, "%Y-%m-%d %H:%M:%S").timetuple())
+        mydate_dec = time.mktime(datetime.datetime.strptime(mydate, "%Y-%m-%d %H:%M:%S").timetuple())
+        now_dec = time.mktime(datetime.datetime.strptime(now, "%Y-%m-%d %H:%M:%S").timetuple())
 
-     active_days = (now_dec - mydate_dec)/60/60/24
-     return int(round(active_days))
-  else:
+        active_days = (now_dec - mydate_dec)/60/60/24
+        return int(round(active_days))
     return -1
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
