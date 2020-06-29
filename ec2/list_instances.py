66a23e9113adbd88bab3976ef6b54700ee55ecc1
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-06-29 17:30:32 +0200

    add ia_instance_profile

--- ec2/list_instances.py
+++ ec2/list_instances.py
@@ -1,24 +1,31 @@
-#!/usr/bin/python
-# This script simply gives a list of the current account EC2 instance with some additionnal details
+'''
+This script simply gives a CSV list of the current account EC2 instances
+'''
+import sys
 import boto3
 import boto3.ec2
-import sys
 
 
 def main(argv):
     ec2 = boto3.resource('ec2')
     instances = ec2.instances.all()
     for instance in instances:
-        print("%s;%s;%s;%s;%s" % (instance.id, get_instance_name(instance),get_instance_stack_name(instance),get_instance_image_id(instance),get_instance_state(instance)))
+        print("%s;%s;%s;%s;%s;%s" % (instance.id,
+                                     get_instance_name(instance),
+                                     get_instance_iam_instance_profile(instance),
+                                     get_instance_stack_name(instance),
+                                     instance.image_id,
+                                     instance.state['Name']))
 
 
 def get_instance_name(ec2_instance):
-    instance_name=''
+    instance_name = ''
     if ec2_instance.tags:
         for tag in ec2_instance.tags:
             if tag["Key"] == 'Name':
                 instance_name = tag["Value"]
     return instance_name
+
 
 def get_instance_stack_name(ec2_instance):
     stack_name = ''
@@ -28,12 +35,12 @@ def get_instance_stack_name(ec2_instance):
                 stack_name = tags["Value"]
     return stack_name
 
-def get_instance_image_id(ec2_instance):
-    return ec2_instance.image_id
 
-def get_instance_state(ec2_instance):
-    return ec2_instance.state['Name']
+def get_instance_iam_instance_profile(ec2_instance):
+    if ec2_instance.iam_instance_profile is not None:
+        return ec2_instance.iam_instance_profile.get("Arn", '')
+    return ""
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
