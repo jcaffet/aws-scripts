4627f0f58df9b0a1cc79bab4baf2b40bee3d0c25
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-06-29 23:27:59 +0200

    syntax improvements

--- asg/list_asg.py
+++ asg/list_asg.py
@@ -1,20 +1,25 @@
-#!/usr/bin/python
-# This script simply gives the list of the current account Auto Scaling Groups (ASG)
+'''
+This script simply gives the list of the current account Auto Scaling Groups
+
+'''
+import sys
 import boto3
 import boto3.ec2
-import sys
 
 
 def main(argv):
 
-   client = boto3.client('autoscaling')
-   response = client.describe_auto_scaling_groups(MaxRecords=100)
-   as_groups = response.get('AutoScalingGroups')
+    client = boto3.client('autoscaling')
+    response = client.describe_auto_scaling_groups()
+    list_items = response.get('AutoScalingGroups')
 
-   for asg in as_groups:
-      print("%s" % (asg.get('AutoScalingGroupName')))
+    while 'LastEvaluatedKey' in response:
+        response = client.describe_auto_scaling_groups(ExclusiveStartKey=response['LastEvaluatedKey'])
+        list_items.extend(response.get('AutoScalingGroups'))
+
+    for asg in list_items:
+        print("%s" % (asg.get('AutoScalingGroupName')))
 
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
--- asg/list_asg_by_tag.py
+++ asg/list_asg_by_tag.py
@@ -1,26 +1,29 @@
-#!/usr/bin/python
-# this script simply gives the list of the current account Auto Scaling Groups (ASG) depending on one key/value tag
-# The script uses JMESPath as query language as query language
-import boto3
+'''
+this script gives the list of the current account Auto Scaling Groups (ASG)
+depending on one key/value tag
+The script uses JMESPath as query language as query language
+'''
 import sys
+import boto3
+
 
 def main(argv):
 
-  TAG_KEY = 'someKey'
-  TAG_VALUE = 'someValue'
+    TAG_KEY = 'someKey'
+    TAG_VALUE = 'someValue'
 
-  client = boto3.client('autoscaling')
-  paginator = client.get_paginator('describe_auto_scaling_groups')
-  page_iterator = paginator.paginate(
-      PaginationConfig={'PageSize': 100}
-  )
+    client = boto3.client('autoscaling')
+    paginator = client.get_paginator('describe_auto_scaling_groups')
+    page_iterator = paginator.paginate(
+        PaginationConfig={'PageSize': 100}
+    )
 
-  filtered_asgs = page_iterator.search(
-      'AutoScalingGroups[] | [?contains(Tags[?Key==`{}`].Value, `{}`)]'.format(TAG_KEY,TAG_VALUE))
-  
-  for asg in filtered_asgs:
-      print asg['AutoScalingGroupName']
+    filtered_asgs = page_iterator.search(
+        'AutoScalingGroups[] | [?contains(Tags[?Key==`{}`].Value, `{}`)]'.format(TAG_KEY,TAG_VALUE))
+
+    for asg in filtered_asgs:
+        print(asg['AutoScalingGroupName'])
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
-
