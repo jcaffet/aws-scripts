6129dd801dd3d9179124f17cc6a2f0fab4b63a10
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-06-29 14:30:23 +0200

    simply refactoring folling pylint

--- iam/list_iam_users.py
+++ iam/list_iam_users.py
@@ -1,17 +1,21 @@
-#!/usr/bin/python
-# This script simply gives a list of the current account IAM users (with some details)
+'''
+This script simply gives a CSV ist of the current account IAM users
+'''
 import boto3
-import sys
 
-def main(argv):
 
-   iam = boto3.resource('iam')
-   users = iam.users.all()
-   print("user_name;user_create_date;user_password_last_used")
-   for user in users:
-      print("%s;%s;%s" % (user.user_name, user.create_date, user.password_last_used))
+def main():
+    '''
+    Simply connects to AWS API with boto3 and prints the result
+    '''
+    iam = boto3.resource('iam')
+    users = iam.users.all()
+    print("user_name;create_date;password_last_used")
+    for user in users:
+        print("%s;%s;%s" % (user.user_name,
+                            user.create_date,
+                            user.password_last_used))
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
-
+    main()
