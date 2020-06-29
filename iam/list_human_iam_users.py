647700f87860393ad7372a3a3e93c8594a5e546f
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-06-29 15:44:23 +0200

    refactor with pylint

--- iam/list_human_iam_users.py
+++ iam/list_human_iam_users.py
@@ -1,24 +1,34 @@
-#!/usr/bin/python
-# This script simply gives a list of human IAM users (IAM users with a password)
-# Need Python >= 3.6
-import boto3
+'''
+This script simply gives a list of human IAM users (IAM users with a password)
+Need Python >= 3.6
+'''
 import sys
 from time import sleep
 import csv
+import boto3
+from botocore.exceptions import ClientError
+
 
 def main(argv):
 
-   iam_client = boto3.client('iam')
-   AwsAccountId = boto3.client('sts').get_caller_identity()['Account']
+    iam_client = boto3.client('iam')
+    awsaccountid = boto3.client('sts').get_caller_identity()['Account']
 
-   credential_report = get_credential_report(iam_client)
-   for row in credential_report:
-     if row['password_enabled'] == "true":
-       print ("%s;%s;%s" % (AwsAccountId, row['user'], row['password_last_used']))
+    credential_report = get_credential_report(iam_client)
+    for row in credential_report:
+        if row['password_enabled'] == "true":
+            print("%s;%s;%s" % (awsaccountid,
+                                row['user'],
+                                row['password_last_used']))
+
 
 def get_credential_report(iam_client):
+    '''
+    Recursive function the generates a credentials report and waits for
+    its final completion
+    '''
     resp = iam_client.generate_credential_report()
-    if resp['State'] == 'COMPLETE' :
+    if resp['State'] == 'COMPLETE':
         try:
             response = iam_client.get_credential_report()
             credential_report_csv = response['Content']
@@ -26,12 +36,13 @@ def get_credential_report(iam_client):
             credential_report = []
             for row in reader:
                 credential_report.append(row)
-            return(credential_report)
+            return credential_report
         except ClientError as e:
-            print("Error getting Report: " + e.message)
+            print("Error getting Report: " + e)
     else:
         sleep(2)
         return get_credential_report(iam_client)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
