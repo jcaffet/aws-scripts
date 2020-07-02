546fe9d77a8db10d8f3e052b48afe3b43248f581
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-07-02 21:41:59 +0200

    externalize some functions

--- iam/list_iam_users_old_password.py
+++ iam/list_iam_users_old_password.py
@@ -2,16 +2,10 @@
 This script simply gives a list of IAM users with a password age old than
 the max defined
 Pay attention that credential reports are renewed by AWS every 4 hours.
-Credential Reports dates are in ISO8601 format like 2017-03-01T09:51:51+00:00
-
-To convert to datetime, with use an out of the box function new in Python 3.7
 '''
 import sys
-from datetime import datetime, timezone
-from time import sleep
-import csv
 import boto3
-from botocore.exceptions import ClientError
+import utils
 
 
 def main(argv):
@@ -20,46 +14,14 @@ def main(argv):
     max_password_age = 180
 
     iam_client = boto3.client('iam')
-    credential_report = get_credential_report(iam_client)
+    credential_report = utils.get_credential_report(iam_client)
 
+    print("user;password_age")
     for row in credential_report:
         if row['password_enabled'] == "true":
-            password_age = number_of_days_since_now(row['password_last_changed'])
+            password_age = utils.number_of_days_since_now(row['password_last_changed'])
             if password_age > max_password_age:
                 print("%s;%s" % (row['user'], password_age))
-
-
-def get_credential_report(iam_client):
-    '''
-    Recursive function the generates a credentials report and waits for
-    its final completion
-    '''
-    resp = iam_client.generate_credential_report()
-    if resp['State'] == 'COMPLETE':
-        try:
-            response = iam_client.get_credential_report()
-            credential_report_csv = response['Content']
-            reader = csv.DictReader(credential_report_csv.decode('utf-8').splitlines())
-            credential_report = []
-            for row in reader:
-                credential_report.append(row)
-            return credential_report
-        except ClientError as e:
-            print("Error getting Report: " + e)
-    else:
-        sleep(2)
-        return get_credential_report(iam_client)
-
-
-def number_of_days_since_now(mydate):
-    '''
-    The incoming mydate is in String type and could contain "no_information"
-    fromisoformat only exists since Python 3.7
-    '''
-    if mydate == "no_information":
-        return mydate
-    else:
-        return (datetime.now(timezone.utc) - datetime.fromisoformat(mydate)).days
 
 
 if __name__ == '__main__':
---
+++ iam/utils.py
@@ -0,0 +1,45 @@
+'''
+Generic usefull methods
+Needs Python>=3.7 for fromisoformat usage
+'''
+from datetime import datetime, timezone
+from time import sleep
+import csv
+from botocore.exceptions import ClientError
+
+
+def get_credential_report(iam_client):
+    '''
+    Recursive function the generates a credentials report and waits for
+    its final completion
+    Credential Reports dates are in ISO8601 format like 2017-03-01T09:51:51+00:00
+    Entries :
+    iam_client : an IAM.Client class
+    '''
+    resp = iam_client.generate_credential_report()
+    if resp['State'] == 'COMPLETE':
+        try:
+            response = iam_client.get_credential_report()
+            credential_report_csv = response['Content']
+            reader = csv.DictReader(credential_report_csv.decode('utf-8').splitlines())
+            credential_report = []
+            for row in reader:
+                credential_report.append(row)
+            return credential_report
+        except ClientError as e:
+            print("Error getting Report: " + e)
+    else:
+        sleep(2)
+        return get_credential_report(iam_client)
+
+
+def number_of_days_since_now(mydate):
+    '''
+    Calculates the number of days between today and the specifed date
+    Entry : string ISO8601 format like 2017-03-01T09:51:51+00:00
+    Returns a integer number
+    '''
+    if mydate == "no_information":
+        return mydate
+    else:
+        return (datetime.now(timezone.utc) - datetime.fromisoformat(mydate)).days
