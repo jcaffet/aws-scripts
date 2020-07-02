6f275aa8079c15fdd1ef3b51d5f74abad928b1ee
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-07-02 22:49:27 +0200

    additional checks

--- report.sh
+++ report.sh
@@ -9,4 +9,6 @@ ${PYTHON_EXEC} ami/list_own_ami.py >report_list_own_ami.csv
 ${PYTHON_EXEC} ami/list_instances_with_unvisible_ami.py >report_list_instances_with_unvisible_ami.csv
 ${PYTHON_EXEC} asg/list_asg.py >report_list_asg.csv
 ${PYTHON_EXEC} ssm/list_managed_instances.py > report_list_managed_instances.csv
-${PYTHON_EXEC} vpc/list_unused_security_groups.py > report_list_unused_security_groups.csv
+${PYTHON_EXEC} vpc/list_unused_security_groups.py > report_list_unused_security_groups.txt
+${PYTHON_EXEC} iam/list_iam_users_old_password.py > report_list_iam_users_old_password.csv
+${PYTHON_EXEC} iam/list_inactive_iam_users.py > report_list_inactive_iam_users.csv
