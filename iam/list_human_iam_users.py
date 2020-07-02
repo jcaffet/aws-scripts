b78c7f572002a5e619fafee2f8b2a59d08bde1fd
jcaffet <15911786+jcaffet@users.noreply.github.com>
2020-07-02 22:48:45 +0200

    externalize credential report call

--- iam/list_human_iam_users.py
+++ iam/list_human_iam_users.py
@@ -1,12 +1,9 @@
 '''
 This script simply gives a list of human IAM users (IAM users with a password)
-Need Python >= 3.6
 '''
 import sys
-from time import sleep
-import csv
 import boto3
-from botocore.exceptions import ClientError
+import utils
 
 
 def main(argv):
@@ -14,34 +11,12 @@ def main(argv):
     iam_client = boto3.client('iam')
     awsaccountid = boto3.client('sts').get_caller_identity()['Account']
 
-    credential_report = get_credential_report(iam_client)
+    credential_report = utils.get_credential_report(iam_client)
     for row in credential_report:
         if row['password_enabled'] == "true":
             print("%s;%s;%s" % (awsaccountid,
                                 row['user'],
                                 row['password_last_used']))
-
-
-def get_credential_report(iam_client):
-    '''
-    Recursive function the generates a credentials report and waits for
-    its final completion
-    '''
-    resp = iam_client.generate_credential_report()
-    if resp['State'] == 'COMPLETE':
-        try:
-            response = iam_client.get_credential_report()
-            credential_report_csv = response['Content']
-            reader = csv.DictReader(credential_report_csv.decode('utf-8').splitlines())
-            credential_report = []
-            for row in reader:
-                credential_report.append(row)
-            return credential_report
-        except ClientError as e:
-            print("Error getting Report: " + e)
-    else:
-        sleep(2)
-        return get_credential_report(iam_client)
 
 
 if __name__ == '__main__':
